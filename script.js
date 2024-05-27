0af58f0b9ad98832f9436e6e74aa3d8af1ee14d5
SuperDev <dzenborg@gmail.com>
2024-05-27 11:16:15 +0400

    вложенные циклы

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+let result = '';
+const length = 7;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Последний просмотренный фильм?',''),
-      b = prompt('Оценка?',''),
-      c = prompt('Последний просмотренный фильм?',''),
-      d = prompt('Оценка?','');
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+for (let i = 1; i < length; i++){
+    for (let j = 0; j < i; j++){
+        result += '*';
+    }
+    result += '\n'
+}
+console.log(result);
